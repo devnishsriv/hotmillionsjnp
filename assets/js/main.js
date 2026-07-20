e21c968cd3a205c54aa140a4430e0fd8a93d28f9
Nishant <realnishant02@gmail.com>
2026-07-20 22:02:18 +0530

    ADDED ANIMATION TO SPECIALITIES SECTION

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,6 +195,29 @@ function initAnimations(){
   
   gsap.registerPlugin(ScrollTrigger);
 
+  // Initialize Lenis for smooth scrolling
+  if (typeof Lenis !== 'undefined') {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }
+
   // Hero text reveal
   if (document.querySelector('.reveal-text .char')) {
     gsap.to('.reveal-text .char',{
